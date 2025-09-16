bc3daaf007d3e244559654434d07490efa929439
bmartin435 <bmartin435@phm.k12.in.us>
2025-09-16 09:46:20 -0400

    Create Dictionaries.py

---
+++ Dictionaries.py
@@ -0,0 +1,6 @@
+# 1. Create a dictionary of 5 countries and their capitals.
+# 2. Print the capital of 2 countries.
+# 3. Add a new country-capital pair.
+# 4. Change the capital of one country.
+# 5. Loop through and print all countries and capitals.
+
